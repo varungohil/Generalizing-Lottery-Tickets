678ac13091c27ea86057f4aedd691eb9ad29455b
Varun <gohil.varun@iitgn.ac.in>
2019-12-01 11:36:14 +0530

    Corrected identation level

--- src/iterative_pruning.py
+++ src/iterative_pruning.py
@@ -17,7 +17,7 @@ def get_20_percent(total):
 	20% of total
 
 	"""
-    return 0.2*total
+	return 0.2*total
 
 
 def get_weight_fractions():
@@ -26,52 +26,52 @@ def get_weight_fractions():
 	"""
 	percent_20s = []
 	for i in range(31):
-	    percent_20s.append(get_20_percent(100 - sum(percent_20s)))
+		percent_20s.append(get_20_percent(100 - sum(percent_20s)))
 	weight_fractions = []
 	for i in range(31):
-	    weight_fractions.append(sum(percent_20s[:i]))
+		weight_fractions.append(sum(percent_20s[:i]))
 	return weight_fractions
 
 
 def permute_masks(old_masks):
-    """ Function to randomly permute the mask in a global manner.
-        Arguments
-        ---------
-        old_masks: List containing all the layer wise mask of the neural network, mandatory. No default.
-        seed: Integer containing the random seed to use for reproducibility. Default is 0
+	""" 
+	Function to randomly permute the mask in a global manner.
+	Arguments
+	---------
+	old_masks: List containing all the layer wise mask of the neural network, mandatory. No default.
+	seed: Integer containing the random seed to use for reproducibility. Default is 0
 
-        Returns
-        -------
-        new_masks: List containing all the masks permuted globally
-        """
+	Returns
+	-------
+	new_masks: List containing all the masks permuted globally
+	"""
 
-    layer_wise_flatten = []                      # maintain the layerwise flattened tensor
-    for i in range(len(old_masks)):
-        layer_wise_flatten.append(old_masks[i].flatten())
+	layer_wise_flatten = []                      # maintain the layerwise flattened tensor
+	for i in range(len(old_masks)):
+		layer_wise_flatten.append(old_masks[i].flatten())
 
-    global_flatten = []
-    for i in range(len(layer_wise_flatten)):
-        if len(global_flatten) == 0:
-            global_flatten.append(layer_wise_flatten[i].cpu())
-        else:
-            global_flatten[-1] = np.append(global_flatten[-1], layer_wise_flatten[i].cpu())
+	global_flatten = []
+	for i in range(len(layer_wise_flatten)):
+		if len(global_flatten) == 0:
+			global_flatten.append(layer_wise_flatten[i].cpu())
+		else:
+			global_flatten[-1] = np.append(global_flatten[-1], layer_wise_flatten[i].cpu())
+	permuted_mask = np.random.permutation(global_flatten[-1])
 
-    permuted_mask = np.random.permutation(global_flatten[-1])
+	new_masks = []
+	idx1 = 0
+	idx2 = 0
+	for i in range(len(old_masks)):
+		till_idx = old_masks[i].numel()
+		idx2 = idx2 + till_idx
+		new_masks.append(permuted_mask[idx1:idx2].reshape(old_masks[i].shape))
+		idx1 = idx2
 
-    new_masks = []
-    idx1 = 0
-    idx2 = 0
-    for i in range(len(old_masks)):
-        till_idx = old_masks[i].numel()
-        idx2 = idx2 + till_idx
-        new_masks.append(permuted_mask[idx1:idx2].reshape(old_masks[i].shape))
-        idx1 = idx2
+	# Convert to tensor
+	for i in range(len(new_masks)):
+		new_masks[i] = torch.tensor(new_masks[i])
 
-    # Convert to tensor
-    for i in range(len(new_masks)):
-        new_masks[i] = torch.tensor(new_masks[i])
-
-    return new_masks
+	return new_masks
 
 
 def prune_iteratively(model, dataloader, architecture, optimizer_type, device, models_path, init_path, random, is_equal_classes):
--- src/utils.py
+++ src/utils.py
@@ -15,79 +15,42 @@ def args_parser_train():
 	"""
 	returns argument parser object used while training a model
 	"""
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--architecture', required=True, type=str,
-                        help='neural network architecture [vgg19, resnet50]') 
-
-    parser.add_argument('--dataset',type=str, required=True,
-                        help='dataset [cifar10, cifar100, svhn, fashionmnist]')
-
-    parser.add_argument('--batch-size', type=int, default=512,
-                        help='input batch size for training (default: 512)')
-
-    parser.add_argument('--optimizer', type=str, default='sgd',
-                        help='optimizer [sgd, adam]')
-
-    parser.add_argument('--seed', type=int, default=1,
-                        help='random seed (default: 1)')
-
-    parser.add_argument('--model-saving-path',  type=str, default = ".",
-                        help='path to directory where you want to save trained models (default = .)')  
-    return parser
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--architecture', required=True, type=str, help='neural network architecture [vgg19, resnet50]') 
+	parser.add_argument('--dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
+	parser.add_argument('--batch-size', type=int, default=512, help='input batch size for training (default: 512)')
+	parser.add_argument('--optimizer', type=str, default='sgd', help='optimizer [sgd, adam]')
+	parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
+	parser.add_argument('--model-saving-path',  type=str, default = ".",help='path to directory where you want to save trained models (default = .)')  
+	return parser
 
 def args_parser_iterprune():
 	"""
 	returns argument parser object used for iterative pruning
 	"""
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--architecture', type=str, metavar='arch',required=True,
-                        help='neural network architecture [vgg19, resnet50]') 
-
-    parser.add_argument('--target-dataset',type=str, required=True,
-                        help='dataset [cifar10, cifar100, svhn, fashionmnist]')
-    parser.add_argument('--batch-size', type=int, default=512,
-                        help='input batch size for training (default: 512)')
-
-    parser.add_argument('--source-dataset',type=str, required=True,
-                        help='dataset [cifar10, cifar100, svhn, fashionmnist]')
-
-    parser.add_argument('--optimizer', type=str, default='sgd',
-                        help='optimizer [sgd, adam]')
-
-    parser.add_argument('--seed', type=int, default=1,
-                        help='random seed (default: 1)')
-
-    parser.add_argument('--init-path',type=str, required=True,
-                        help='path to winning initialization model (default = .)')  
-  
-    parser.add_argument('--model-saving-path', type=str, default = ".",
-                        help='path to directory where you want to save trained models (default = .)')  
-
-    parser.add_argument('--random', type=str,  default = "false",
-                        help='to train random ticket (default = false)')
-    return parser
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--architecture', type=str, metavar='arch',required=True, help='neural network architecture [vgg19, resnet50]') 
+	parser.add_argument('--target-dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
+	parser.add_argument('--batch-size', type=int, default=512,help='input batch size for training (default: 512)')
+	parser.add_argument('--source-dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
+	parser.add_argument('--optimizer', type=str, default='sgd', help='optimizer [sgd, adam]')
+	parser.add_argument('--seed', type=int, default=1, help='random seed (default: 1)')
+	parser.add_argument('--init-path',type=str, required=True, help='path to winning initialization model (default = .)')  
+	parser.add_argument('--model-saving-path', type=str, default = ".", help='path to directory where you want to save trained models (default = .)')
+	parser.add_argument('--random', type=str,  default = "false", help='to train random ticket (default = false)')
+	return parser
 
 
 def args_parser_test():
 	"""
 	returns argument parser object used while testing a model
 	"""
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--architecture', type=str, metavar='arch', required=True,
-                        help='neural network architecture [vgg19, resnet50]') 
-
-    parser.add_argument('--dataset',type=str, required=True,
-                        help='dataset [cifar10, cifar100, svhn, fashionmnist]')
-
-    parser.add_argument('--batch-size', type=int, default=512,
-                        help='input batch size for training (default: 512)')
-
-    parser.add_argument('--model_path',type=str, required=True,
-                        help='path to the model for finding test accuracy')  
-    return parser
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--architecture', type=str, metavar='arch', required=True, help='neural network architecture [vgg19, resnet50]') 
+	parser.add_argument('--dataset',type=str, required=True, help='dataset [cifar10, cifar100, svhn, fashionmnist]')
+	parser.add_argument('--batch-size', type=int, default=512, help='input batch size for training (default: 512)')
+	parser.add_argument('--model_path',type=str, required=True, help='path to the model for finding test accuracy')
+	return parser
 
 def load_dataset(dataset, batch_size = 512, is_train_split=True):
 	"""

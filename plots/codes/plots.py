1852ace53c760f102cb4ba123e7d9ae1c96c1515
S Deepak Narayanan <deepaknarayanan120199@gmail.com>
2020-03-28 21:39:45 +0530

    Missed out latexify Code borrowed from @nipunbatra

---
+++ plots/codes/plots.py
@@ -0,0 +1,69 @@
+from math import sqrt
+import matplotlib 
+import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd 
+def latexify(fig_width=None, fig_height=None, columns=1):
+    """Set up matplotlib's RC params for LaTeX plotting.
+    Call this before plotting a figure.
+
+    Parameters
+    ----------
+    fig_width : float, optional, inches
+    fig_height : float,  optional, inches
+    columns : {1, 2}
+    """
+
+    # code adapted from http://www.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
+
+    # Width and max height in inches for IEEE journals taken from
+    # computer.org/cms/Computer.org/Journal%20templates/transactions_art_guide.pdf
+
+    assert(columns in [1,2])
+
+    if fig_width is None:
+        fig_width = 3.39 if columns==1 else 6.9 # width in inches
+
+    if fig_height is None:
+        golden_mean = (sqrt(5)-1.0)/2.0    # Aesthetic ratio
+        fig_height = fig_width*golden_mean # height in inches
+
+    MAX_HEIGHT_INCHES = 8.0
+    if fig_height > MAX_HEIGHT_INCHES:
+        print("WARNING: fig_height too large:" + fig_height + 
+              "so will reduce to" + MAX_HEIGHT_INCHES + "inches.")
+        fig_height = MAX_HEIGHT_INCHES
+
+    params = {'backend': 'ps',
+              'text.latex.preamble': [r'\usepackage{gensymb}'],
+              'axes.labelsize': 14, # fontsize for x and y labels (was 10)
+              'axes.titlesize': 14,
+              'font.size': 14, # was 10
+              'legend.fontsize': 14, # was 10
+              'xtick.labelsize': 14,
+              'ytick.labelsize': 14,
+              'text.usetex': True,
+              'figure.figsize': [fig_width,fig_height]
+#               'font.family': 'serif'
+    }
+
+    matplotlib.rcParams.update(params)
+
+
+def format_axes(ax):
+    SPINE_COLOR = 'gray'
+
+    for spine in ['top', 'right']:
+        ax.spines[spine].set_visible(False)
+
+    for spine in ['left', 'bottom']:
+        ax.spines[spine].set_color(SPINE_COLOR)
+        ax.spines[spine].set_linewidth(0.5)
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    for axis in [ax.xaxis, ax.yaxis]:
+        axis.set_tick_params(direction='out', color=SPINE_COLOR)
+
+    return ax
